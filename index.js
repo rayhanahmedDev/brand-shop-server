5f6e163d860389fd393b8acb6550e333024648a4
rayhan9802 <mdrayhaanahmed@gmail.com>
2023-10-21 15:06:32 +0600

    get data form database and show show ui

--- index.js
+++ index.js
@@ -28,6 +28,61 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const userCollection = client.db("brandDB").collection('user')
+    const brandCollection = client.db("brandDB").collection('brand')
+    const addCardCollection = client.db("brandDB").collection('addcard')
+
+    // products
+    app.get('/brand',async(req,res) =>{
+      const result = await brandCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/brand/:brand', async(req,res) =>{
+      const brandNames = req.params.brand;
+      const brands = await brandCollection.find().toArray()
+
+      // const query = {brand:brandNames}
+      // const result =await brandCollection.find(query).toArray()
+      const result= await brands.filter( brand => brand.brand === brandNames)
+      res.send(result)
+    })
+
+    // brand id form
+    app.get('/brandadd/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await brandCollection.findOne(query)
+      res.send(result)
+    })
+
+    // update brand product
+    app.put('/brandup/:id',async(req,res) =>{
+      const id = req.params.id
+      const data = req.body
+      const filter = {_id : new ObjectId(id)}
+      const options = { upsert: true };
+      const updateProduct = {
+        $set : {
+          name : data.name, 
+          brand : data.brand, 
+          computer : data.computer, 
+          price : data.price, 
+          rating : data.rating, 
+          photo : data.photo
+        }
+      }
+      const result = await brandCollection.updateOne(filter,updateProduct,options)
+      res.send(result)
+    })
+
+    // 2nd post section
+    app.post("/addcard", async (req, res) => {
+      const user = req.body;
+      const result = await addCardCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // main post section
 
     app.post('/users',async(req,res) =>{
         const user = req.body;
@@ -46,7 +101,7 @@ async function run() {
       const result = await userCollection.deleteOne(query)
       res.send(result)
     })
-    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
